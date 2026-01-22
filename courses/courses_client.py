3ccb6a16ea59adf202336a6bd0ead9e2356ddab1
yakov17 <yakovkuz17@gmail.com>
2026-01-22 19:31:03 +0300

    Добавил ещё api clients

---
+++ courses/courses_client.py
@@ -0,0 +1,82 @@
+from clients.api_client import APIClient
+from httpx import Response
+from typing import TypedDict
+
+class GetCoursesQueryDict(TypedDict):
+    """
+        Описание структуры запроса на получение списка курсов.
+        """
+    query : str
+
+class CreateCourseRequestDict(TypedDict):
+    """
+       Описание структуры запроса на создание курса.
+       """
+    title : str
+    maxScore : str
+    minScore : str
+    description : str
+    estimatedTime : str
+    previewFileId : str
+    createdByUserId : str
+
+class UpdateCourseRequestDict(TypedDict):
+    """
+       Описание структуры запроса на обновление курса.
+       """
+    title: str | None
+    maxScore: int | None
+    minScore: int | None
+    description: str | None
+    estimatedTime: str | None
+
+class CoursesClient(APIClient):
+    """
+       Клиент для работы с /api/v1/courses
+       """
+    def get_courses_api(self, query : GetCoursesQueryDict) -> Response:
+        """
+                Метод получения списка курсов.
+
+                :param query: Словарь с userId.
+                :return: Ответ от сервера в виде объекта httpx.Response
+                """
+        return self.get('api/v1/courses', params = query)
+
+    def get_course_api(self, course_id : str) -> Response:
+        """
+                Метод получения курса.
+
+                :param course_id: Идентификатор курса.
+                :return: Ответ от сервера в виде объекта httpx.Response
+                """
+        return self.get(f'api/v1/courses/{course_id}')
+
+    def create_course_api(self, reqeust : CreateCourseRequestDict) -> Response:
+        """
+                Метод создания курса.
+
+                :param request: Словарь с title, maxScore, minScore, description, estimatedTime,
+                previewFileId, createdByUserId.
+                :return: Ответ от сервера в виде объекта httpx.Response
+                """
+        return self.post('api/v1/courses', data = reqeust)
+
+    def update_courses_api(self, exercise_id : str, request : UpdateCourseRequestDict) -> Response:
+        """
+                Метод обновления курса.
+
+                :param course_id: Идентификатор курса.
+                :param request: Словарь с title, maxScore, minScore, description, estimatedTime.
+                :return: Ответ от сервера в виде объекта httpx.Response
+                """
+        return self.patch(f'api/v1/exercises/{exercise_id}', request)
+
+    def delete_course_api(self, exercise_id : str) -> Response:
+        """
+                Метод удаления курса.
+
+                :param course_id: Идентификатор курса.
+                :return: Ответ от сервера в виде объекта httpx.Response
+                """
+        return self.delete(f'api/v1/exercises/{exercise_id}')
---
+++ httpx/httpx_create_file.py
@@ -0,0 +1,43 @@
+import httpx
+
+from tools.fakers import get_random_email
+
+# Создаем пользователя
+create_user_payload = {
+    "email": get_random_email(),
+    "password": "string",
+    "lastName": "string",
+    "firstName": "string",
+    "middleName": "string"
+}
+create_user_response = httpx.post("http://localhost:8000/api/v1/users", json=create_user_payload)
+create_user_response_data = create_user_response.json()
+print('Create user data:', create_user_response_data)
+
+# Проходим аутентификацию
+login_payload = {
+    "email": create_user_payload['email'],
+    "password": create_user_payload['password']
+}
+login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json=login_payload)
+login_response_data = login_response.json()
+print('Login data:', login_response_data)
+
+create_file_headers = {
+    "Authorization": f"Bearer {login_response_data['token']['accessToken']}"
+}
+import os
+print(os.getcwd())
+
+create_file_response = httpx.post(
+    "http://localhost:8000/api/v1/files",
+    headers = create_file_headers,
+    data = {
+        "filename" : "test.png",
+        "directory": "courses"
+    },
+    files =
+    {'upload_file': open(r"C:\Users\Яков\Desktop\Stepik\autotests\testdata\courses\test.png", 'rb')}
+)
+create_file_response_data = create_file_response.json()
+print('Create file data:', create_file_response_data)
