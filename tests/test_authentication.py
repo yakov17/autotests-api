97e0cb4de71c61e998c43a42aa6465f07b2f70e4
Кузнецов Яков Алексеевич <YAKuznetsov@tsc.ts>
2026-02-16 17:20:20 +0300

    markers

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -7,8 +7,10 @@ from clients.users.users_schema import CreateUserRequestSchema
 from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
+import pytest
 
-
+@pytest.mark.regression
+@pytest.mark.authentication
 def test_login():
     public_users_client = get_public_users_client()
     authentication_client = get_authentication_client()
--- tests/test_users.py
+++ tests/test_users.py
@@ -5,8 +5,10 @@ from http import HTTPStatus
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response
+import pytest
 
-
+@pytest.mark.users
+@pytest.mark.regression
 def test_create_user():
     public_users_client = get_public_users_client()
     request = CreateUserRequestSchema()
